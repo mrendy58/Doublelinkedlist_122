16abbf6fc662e08f423960ca1f12b14378767666
mrendy58 <derendyyy58@gmail.com>
2023-05-25 15:55:51 +0700

    membuat new node step 3

--- Doublelinkedlist_122/Doublelinkedlist_122.cpp
+++ Doublelinkedlist_122/Doublelinkedlist_122.cpp
@@ -38,7 +38,16 @@ void DoubleLinkedList::addNode() {
     cin >> nm;
     Node* newNode = new Node(); //step 1
     newNode->noMhs = nim; //step 2
-    newNode->name = nm; //step 3
+    newNode->name = nm; //step 2
+
+    //*insert a node in the begining of a doubly - linked list*/
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs) {
+            cout << "\nDuplicate number not allowed" << endl;
+            return;
+        }
+        newNode->next = START; // step 3
+    }
 
 }
 
