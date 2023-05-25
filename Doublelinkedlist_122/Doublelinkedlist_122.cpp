9a11287f7e8a1d73cfa080ed294b84ab7d99d641
mrendy58 <derendyyy58@gmail.com>
2023-05-25 16:16:06 +0700

    jika current next tdk sama dengan null maka akan lanjut ke noMhs

--- Doublelinkedlist_122/Doublelinkedlist_122.cpp
+++ Doublelinkedlist_122/Doublelinkedlist_122.cpp
@@ -60,7 +60,15 @@ void DoubleLinkedList::addNode() {
     while (current->next != NULL && current->next->noMhs < nim) // step 1.c
     {
         previous = current; // step 1.d
+        current = current->next; // step 1.e
     }
+
+    if (current->next != NULL && nim == current->next->noMhs) {
+        cout << "\nDuplicate roll numbers not allowed" << endl;
+        return;
+    }
+
+
 
 }
 
