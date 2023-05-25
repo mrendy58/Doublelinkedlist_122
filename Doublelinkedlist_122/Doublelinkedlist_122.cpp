cdabc6efa49388df2bfd40d424e138068a8c1f01
mrendy58 <derendyyy58@gmail.com>
2023-05-25 15:58:28 +0700

    membuat new node step 5

--- Doublelinkedlist_122/Doublelinkedlist_122.cpp
+++ Doublelinkedlist_122/Doublelinkedlist_122.cpp
@@ -49,6 +49,7 @@ void DoubleLinkedList::addNode() {
         newNode->next = START; // step 3
         if (START != NULL)
             START->prev = newNode; // step 4
+        newNode->prev = NULL; // step 5
     }
 
 }
